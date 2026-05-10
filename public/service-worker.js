680394f5875c1be0c98f822aa2820b60f0105445
Dylan Bergozza <dylan.bergozza@live.fr>
2026-05-11 00:50:30 +0200

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -114,10 +114,23 @@ const cacheFirst = async (request) => {
   }
 };
 
+const precacheAppShell = async () => {
+  const cache = await caches.open(APP_CACHE);
+
+  await Promise.allSettled(
+    APP_SHELL_URLS.map(async (url) => {
+      const response = await fetch(url, { cache: 'reload' });
+
+      if (response.ok) {
+        await cache.put(url, response);
+      }
+    })
+  );
+};
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(APP_CACHE)
-      .then((cache) => cache.addAll(APP_SHELL_URLS))
+    precacheAppShell()
       .then(() => self.skipWaiting())
   );
 });
